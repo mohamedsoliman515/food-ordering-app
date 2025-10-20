29fef10d704275ea435a3c1b1d3ae4e27c57ea82
mohamed suliman <mohamedelmoregy515@gmail.com>
2025-10-20 11:18:21 +0200

    Add init com Footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import About from "@/components/about";
 import BestSellers from "./_components/BestSellers";
 import Hero from "./_components/Hero";
+import Contact from "@/components/contact";
 
 export default async function Home() {
   return (
@@ -8,6 +9,7 @@ export default async function Home() {
       <Hero />
       <BestSellers />
       <About />
+      <Contact />
     </main>
   );
 }
---
+++ src/components/footer/index.tsx
@@ -0,0 +1,11 @@
+const Footer = async () => {
+  return (
+    <footer className="border-t p-8 text-center text-accent">
+      <div className="container">
+        <p>Lorem ipsum dolor sit amet.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
