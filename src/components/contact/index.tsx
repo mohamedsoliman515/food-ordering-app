a6a6f4910ff3d56ee9dad45e3cfb182d38b3a788
mohamed suliman <mohamedelmoregy515@gmail.com>
2025-10-20 10:39:29 +0200

    Add init contact page & com

---
+++ src/components/contact/index.tsx
@@ -0,0 +1,22 @@
+import MainHeading from "@/components/main-heading";
+import { Routes } from "@/constants/enums";
+
+const Contact = async () => {
+  return (
+    <section className="section-gap" id={Routes.CONTACT}>
+      <div className="container text-center">
+        <MainHeading
+          subTitle="contact Don'tHesitate"
+          title="contact.contactUs"
+        />
+        <div className="mt-8">
+          <a className="text-4xl underline text-accent" href="tel:+2012121212">
+            +201286490142
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
