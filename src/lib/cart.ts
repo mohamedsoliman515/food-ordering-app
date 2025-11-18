5c1eb2d763d3a6023869d6594024e903f42b7aa0
mohamed suliman <mohamedelmoregy515@gmail.com>
2025-11-18 08:23:41 +0200

    Add fun getItemQuantity in cart

--- src/lib/cart.ts
+++ src/lib/cart.ts
@@ -3,3 +3,6 @@ import { CartItem } from "@/redux/features/cart/cartSlice";
 export const getCartQuantity = (cart: CartItem[]) => {
   return cart.reduce((total, item) => total + (item.quantity || 1), 0);
 };
+export const getItemQuantity = (id: string, cart: CartItem[]) => {
+  return cart.find((item) => item.id === id)?.quantity || 0;
+};
