40fa4c4280bd6c891be4ed15a3032a8da766fdf8
mohamed suliman <mohamedelmoregy515@gmail.com>
2025-11-18 07:05:26 +0200

    show items in cart

--- src/app/cart/_components/CartItems.tsx
+++ src/app/cart/_components/CartItems.tsx
@@ -1,12 +1,72 @@
 "use client";
+import { formatCurrency } from "@/lib/formatCurrency";
 import { selectCartItems } from "@/redux/features/cart/cartSlice";
 import { useAppSelector } from "@/redux/hooks";
+import Image from "next/image";
 
 const CartItems = () => {
   const cart = useAppSelector(selectCartItems);
-  console.log(cart);
 
-  return <div>cartItem</div>;
+  return (
+    <div>
+      {cart && cart.length > 0 ? (
+        <>
+          <ul>
+            {cart.map((item) => (
+              <li key={item.id}>
+                <div className="flex flex-col md:flex-row gap-6 justify-between">
+                  <div className="flex items-center gap-2">
+                    <div className="relative w-24 h-24">
+                      <Image
+                        src={item.image}
+                        className="object-contain"
+                        alt={item.name}
+                        fill
+                      />
+                    </div>
+                    <div>
+                      <h4 className="font-semibold md:text-lg">{item.name}</h4>
+                      <div className="relative">
+                        {item.size && (
+                          <span className="text-sm text-accent">
+                            Size: {item.size.name}
+                          </span>
+                        )}
+                        {item.extras && item.extras.length > 0 && (
+                          <div className="flex gap-1">
+                            <span>Extras:</span>
+                            <ul>
+                              {item.extras.map((extra) => (
+                                <li key={extra.id}>
+                                  <span className="text-sm text-accent">
+                                    {extra.name} {formatCurrency(extra.price)}
+                                  </span>
+                                </li>
+                              ))}
+                            </ul>
+                          </div>
+                        )}
+                        <span className="absolute right-0 top-0 text-sm text-black">
+                          x{item.quantity}
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="flex-1 flex items-center gap-4 justify-end">
+                    <strong className="text-black ">
+                      {formatCurrency(item.basePrise)}
+                    </strong>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </>
+      ) : (
+        <p className="text-accent">There are no items in your cart. Add some</p>
+      )}
+    </div>
+  );
 };
 
 export default CartItems;
