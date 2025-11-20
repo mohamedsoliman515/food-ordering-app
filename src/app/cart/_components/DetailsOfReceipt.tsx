121c581b71bac34e067d10b4dda408d295eb9fd0
mohamed suliman <mohamedelmoregy515@gmail.com>
2025-11-20 02:55:33 +0200

    create getSubTotal

--- src/app/cart/_components/DetailsOfReceipt.tsx
+++ src/app/cart/_components/DetailsOfReceipt.tsx
@@ -8,18 +8,20 @@ const DetailsOfReceipt = ({
   deliveryFee: number;
 }) => {
   return (
-    <div className="flex flex-col justify-end items-end pt-6">
+    <div className="flex flex-col justify-end  pt-6 border border-accent p-4 rounded-[8px]">
       <span className="text-accent font-medium">
         Subtotal:
-        <strong className="text-black">{formatCurrency(subTotal)}</strong>
+        <strong className="text-black  pl-1">{formatCurrency(subTotal)}</strong>
       </span>
       <span className="text-accent font-medium">
         Delivery:
-        <strong className="text-black">{formatCurrency(deliveryFee)}</strong>
+        <strong className="text-black pl-1">
+          {formatCurrency(deliveryFee)}
+        </strong>
       </span>
       <span className="text-accent font-medium">
         Total:
-        <strong className="text-black">
+        <strong className="text-black pl-1">
           {formatCurrency(subTotal + deliveryFee)}
         </strong>
       </span>
--- src/lib/cart.ts
+++ src/lib/cart.ts
@@ -3,6 +3,18 @@ import { CartItem } from "@/redux/features/cart/cartSlice";
 export const getCartQuantity = (cart: CartItem[]) => {
   return cart.reduce((total, item) => total + (item.quantity || 1), 0);
 };
+
 export const getItemQuantity = (id: string, cart: CartItem[]) => {
   return cart.find((item) => item.id === id)?.quantity || 0;
+};
+
+export const getSubTotal = (cart: CartItem[]) => {
+  return cart.reduce((total, item) => {
+    const sizePrice = item.size ? item.size.price : 0;
+    const extras = item.extras?.reduce((sum, extra) => sum + extra.price, 0);
+    const itemTotal =
+      (item.basePrice + sizePrice + (extras || 0)) * (item.quantity || 1);
+
+    return total + itemTotal;
+  }, 0);
 };
